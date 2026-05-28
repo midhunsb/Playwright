c435f2bac16035717a6a7002fb93d7a5f816db3d
Midhun <midhun.sb@duk.ac.in>
2026-05-28 22:27:56 +0530

    Updated final marklist

--- tests/snoumarklist.spec.js
+++ tests/snoumarklist.spec.js
@@ -1,4 +1,4 @@
-const { test, expect } = require('@playwright/test');
+const {test,expect} = require('@playwright/test');
 const fs = require('fs');
 const path = require('path');
 test.setTimeout(600000);
@@ -10,30 +10,34 @@ const csvPath = path.join(__dirname, '../test-data/sgoustudentsmarklist.csv');
 // Read CSV
 const csvData = fs.readFileSync(csvPath, 'utf-8');
 // Convert CSV rows to array
-const users = csvData.split('\n').slice(1).map(row => row.trim()).filter(row => row.length > 0);
+const users = csvData
+  .split('\n').slice(1).map(row => row.trim()).filter(row => row.length > 0);
 users.forEach((email, index) => {
-  test(`SGOU students login ${index + 1}`, async ({ page }) => {
+  test(`SGOU students login  ${index + 1}`, async ({
+    page
+  }) => {
     await page.goto('https://erpsgou.cdipd.in/login-candidate');
-    await page.locator('#logName').fill(email);
-    await page.locator('#psWd').fill('654321');
-    await page.locator('#btnLogin').click();
+    await page.locator('//*[@id="logName"]').fill(email);
+    await page.locator('//*[@id="psWd"]').fill('654321');
+    await page.locator('//*[@id="btnLogin"]').click();
     console.log(`User ${email} logged successfully...`);
+    //await page.waitForTimeout(4000);
     await page.waitForLoadState('networkidle');
-    await page.locator('#course-pills-tab-2-enrld').click();
-    await page.locator('//h6[text()="Grade Card Report"]').click();
+    await page.locator('//*[@id="course-pills-tab-2-enrld"]').click();
+    await page.locator('//h6[@class="card-title" and text()="Grade Card Report"]').click();
     console.log("Grade card report button clicked successfully....");
+    //await page.waitForTimeout(9000);
     const context = page.context();
-   const [newPage] = await Promise.all([
-  context.waitForEvent('page'),
-  page.locator('(//*[@id="semester_list"]//a[text()="Generate"])[1]').click(),
-  console.log("Generate button clicked successfully....")
-]);
-
-await newPage.waitForLoadState('domcontentloaded');
-
-await expect(newPage).toHaveURL(/student-mark-list/);
-
-console.log("PDF page opened successfully...");
-console.log(await newPage.url());
-});
+    const [newPage] = await Promise.all([
+      context.waitForEvent('page'),
+      page.locator('(//*[@id="semester_list"]//a[text()="Generate"])[1]').click(),
+      console.log("Generate button clicked successfully....")
+    ]);
+      await newPage.waitForLoadState('networkidle');
+    await newPage.waitForLoadState('domcontentloaded');
+    //await expect(newPage.locator('embed')).toBeVisible({timeout: 20000});
+    const pdfUrl = await newPage.url();
+    expect(pdfUrl).toContain('student-mark-list');
+    console.log(await newPage.url());
+  });
 });
