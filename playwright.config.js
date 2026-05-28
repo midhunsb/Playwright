0e3043b574a5c7a8fc451d90d16044de0965ba1b
Midhun <midhun.sb@duk.ac.in>
2026-05-28 22:33:39 +0530

    Updated marklist coded

--- playwright.config.js
+++ playwright.config.js
@@ -1,17 +1,30 @@
 const { defineConfig, devices } = require('@playwright/test');
+
 module.exports = defineConfig({
+
   testDir: './tests',
+
   timeout: 600000,
+
   reporter: [
     ['html', { open: 'never' }]
   ],
+
   use: {
+
     ...devices['Desktop Chrome'],
+
+    // Local = headed
+    // GitHub Actions = headless
     headless: process.env.CI ? true : false,
+
     screenshot: 'only-on-failure',
+
     video: 'retain-on-failure',
+
     trace: 'retain-on-failure'
   },
+
   projects: [
     {
       name: 'chromium',
@@ -20,4 +33,5 @@ module.exports = defineConfig({
       },
     },
   ],
+
 });
